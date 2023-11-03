3e222d9efc81f670fc43d0c538602712e3258306
MaxQuin1 <maximoquinteroescobar8@gmail.com>
2023-11-03 11:45:26 -0600

    Registro funcional

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -18,13 +18,13 @@ export default function Login() {
       console.log('Prueba con otro correo o contraseña')
     }
   };
+
   return (
     <>
     <div className="grid grid-cols-1 sm:grid-cols-2 h-screen w-full">
       <div className="hidden sm:block ">
       <img className="w-full h-full object-cover" src='https://elglobal.es/wp-content/uploads/2022/03/GettyImages-1311641109.jpg' alt=''/>
       </div>
-
       <div className="flex flex-col justify-center bg-sky-500">
         <form className="max-w-[400px] w-full mx-auto p-8 px-8 rounded-lg bg-white">
           <h2 className="text-3xl text-center font-bold">Iniciar de sesion</h2>
@@ -48,7 +48,6 @@ export default function Login() {
                   }}
             ></input>
           </div>
-          
           <div className="mb-6 text-center mt-4 ">
               <button 
               className="rounded-lg w-20 h-10 p-0 m-2 bg-green-400"
@@ -56,9 +55,9 @@ export default function Login() {
                 <p className='text-center '>Login</p>
                 </button>
             <p>¿Eres nuevo?</p>
-             {/* <Link to="/Registro"> */}
+             <Link to="/Registro">
                <button className="bg-sky-100 p-3 rounded-lg m-2">Register</button>
-            {/* </Link> */}
+            </Link>
           </div>
         </form>
       </div>
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "../pages/Login";
 import Home from "../pages/Home";
+import Registro from "../pages/Registro";
 
 export const rutas = createBrowserRouter([
   {
     path: "/",
     element: <Login />,
+  },
+  {
+    path: "/registro",
+    element: <Registro />,
   },
   {
     path: "/home",
