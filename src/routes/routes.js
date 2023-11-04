57aea7ff85ecaa51b838bf83b702b19a4f8a30a7
Goraz23 <angomezs.2303@gmail.com>
2023-11-03 20:18:03 -0500

    Se reviso y ya todo está bien jejeje

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "../pages/Login";
 import Home from "../pages/Home";
-<<<<<<< HEAD
+
 import Medicamento from "../pages/Medicamento";
-=======
+
 import Registro from "../pages/Registro";
->>>>>>> master
+
 
 export const rutas = createBrowserRouter([
   {
