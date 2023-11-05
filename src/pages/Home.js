7406cfc0f3f3ee95c3fcad617e61930c9a94787c
MaxQuin1 <maximoquinteroescobar8@gmail.com>
2023-11-05 08:49:42 -0600

    ya se pueden ver los datos de la receta en consola solo falta mapearlo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
 import sol from "../img/sol.png";
 import evening from "../img/evening.png";
 import amanecer from "../img/amanecer.png";
@@ -8,16 +10,104 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   const id_usuario = window.location.href.split("/")[4];
+  const [recetas, setRecetas] = useState([]);
+
+  useEffect(() => {
+    async function fetchRecetas() {
+      try {
+        const response = await axios.get(
+          `http://localhost:8082/verReceta/${id_usuario}`
+        );
+        setRecetas(response.data);
+      } catch (error) {
+        console.error("Error al obtener los datos:", error);
+      }
+    }
+    fetchRecetas();
+  }, []);
+
+  const nuevasFechas = recetas.map((receta) => {
+    const fechaInicial = new Date(receta.fecha);
+    const intervaloEnMilisegundos = receta.intervalo * 3600000;
+    const fechasReceta = [];
+    let fechaActual = fechaInicial;
+
+    for (let i = 0; i <= receta.dias; i++) {
+      fechasReceta.push(new Date(fechaActual));
+      fechaActual = new Date(fechaActual.getTime() + intervaloEnMilisegundos);
+    }
+
+    return fechasReceta;
+  });
+
+  const horarios = [
+    {
+      nombre: "morning",
+      hora_inicio: 0.0,
+      hora_final: 5.59,
+    },
+    {
+      nombre: "noon",
+      hora_inicio: 6.0,
+      hora_final: 11.59,
+    },
+    {
+      nombre: "tarde",
+      hora_inicio: 12.0,
+      hora_final: 17.59,
+    },
+    {
+      nombre: "night",
+      hora_inicio: 18.0,
+      hora_final: 23.59,
+    },
+  ];
+
+  const recetasConHorario = [];
+
+  for (let index = 0; index < recetas.length; index++) {
+    const receta = recetas[index];
+    const fechasReceta = nuevasFechas[index];
+    const fechasConHorario = [];
+
+    for (const fecha of fechasReceta) {
+      let nombreHorario = "";
+
+      if (fecha instanceof Date && !isNaN(fecha)) {
+        const horaFecha = fecha.getHours() + fecha.getMinutes() / 60;
+
+        for (const horario of horarios) {
+          if (
+            horaFecha >= horario.hora_inicio &&
+            horaFecha <= horario.hora_final
+          ) {
+            nombreHorario = horario.nombre;
+            break;
+          }
+        }
+      }
+
+      fechasConHorario.push({
+        fecha,
+        nombre_horario: nombreHorario,
+      });
+    }
+
+    recetasConHorario.push({
+      ...receta,
+      fechasConHorario,
+    });
+  }
+
+  // console.log(recetasConHorario);
 
   return (
     <>
       <div className="bg-blue-200">
         <div dir="rtl" className="b-0">
           <button className="align-top rounded-bl-lg w-[55px] h-[55px] bg-blue-400 mt-0 ml-8 mb-0 text-top">
-            <Link to= {`/medicamento/${id_usuario}`}>
-              <p className="text-2xl ">
-                +
-              </p>
+            <Link to={`/medicamento/${id_usuario}`}>
+              <p className="text-2xl ">+</p>
             </Link>
           </button>
         </div>
@@ -26,12 +116,12 @@ export default function Home() {
             <div className="m-5 border ">
               <table className="w-full ">
                 <tr className="text-center text-xl">
-                  <td></td>
-                  <td>Medications</td>
-                  <td>Dosage</td>
-                  <td>Time</td>
-                  <td>Date</td>
-                  <td>Comments</td>
+                  <th></th>
+                  <th>Medications</th>
+                  <th>Dosage</th>
+                  <th>Time</th>
+                  <th>Date</th>
+                  <th>Comments</th>
                 </tr>
                 <colgroup>
                   <col style={{ borderRight: "2px solid #FFFFFF" }} />
@@ -404,9 +494,6 @@ export default function Home() {
             </div>
           </div>
         </div>
-        {/* <div className="bg-green-100 w-[10%] text-center border rounded-xl ml-[10%] border-green-600 ">
-          <button className="p-1 ">Receta</button>
-        </div> */}
       </div>
     </>
   );
