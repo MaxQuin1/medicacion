a9ac527d33347ed4ec9d0390364515fb8fb6880e
Goraz23 <angomezs.2303@gmail.com>
2023-11-23 11:06:03 -0500

    Se hace una edición en tabas pero aun no se acompleta

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { rutas } from './routes/routes'
 import 'boxicons'
 import './index.css'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={rutas}/>
